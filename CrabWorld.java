3830a42d4259b27362e6e13fd6f07e090bc8fc13
Evan Egan <ee42425@srcs.org>
2021-08-25 17:44:52 -0700

    Completed activity 1
    Video told me to

--- CrabWorld.java
+++ CrabWorld.java
@@ -6,8 +6,20 @@ public class CrabWorld extends World
      * Create the crab world (the beach). Our world has a size 
      * of 560x560 cells, where every cell is just 1 pixel.
      */
+    //रूप में माना जाता है
     public CrabWorld() 
     {
         super(560, 560, 1);
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Crab crab = new Crab();
+        addObject(crab,194,204);
     }
 }
